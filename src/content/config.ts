4ef486e7c5906ccbf694387eb27bfc443b4a61a8
jstamb <pyfooty@gmail.com>
2026-02-10 15:57:57 -0800

    [Feature] Add content collection config

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+
+const caseStudies = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.string(),
+    category: z.string().optional(),
+  }),
+});
+
+export const collections = {
+  'case-studies': caseStudies,
+};
